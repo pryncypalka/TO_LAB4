d9293802db50a1e93f63b78eef0e026def86868a
Jacek Kamiński <jacus502@gmail.com>
2023-12-31 19:30:42 +0100

    cp done

---
+++ commands/cp.py
@@ -0,0 +1,22 @@
+from commands.CommandExecutor import CommandExecutor
+from tree_structure.File import File
+
+
+class Cp(CommandExecutor):
+    def __init__(self):
+        pass
+
+    def execute(self, args, current_directory):
+        destination_directory = args[0]
+        source_filename = args[1]
+        source_file = args[2]
+        destination_path = args[3]
+
+        if source_file is not None and isinstance(source_file, File):
+            if destination_directory.get_children_by_name(source_filename) is not None:
+                return f"File '{source_filename}' already exists in '{destination_path}'."
+            else:
+                destination_directory.add_child(File(source_filename, destination_directory, source_file.get_content()))
+            return f"File '{source_filename}' copied to '{destination_path}'."
+        else:
+            return f"File '{source_filename}' not found."
--- commands/cpProxy.py
+++ commands/cpProxy.py
@@ -1,10 +1,29 @@
 from commands.CommandExecutor import CommandExecutor
+from tree_structure.File import File
+from Path_to_Directory import Path_to_Directory
+from commands.cp import Cp
 
 
 class CpProxy(CommandExecutor):
-    def __init__(self, cp_command):
-        self._cp_command = cp_command
+    def __init__(self):
+        self._cp_command = Cp()
 
     def execute(self, args, current_directory):
+        source_path = args[0]
+        destination_path = args[1]
+        # Resolve the source and destination directories
+        directory_path = Path_to_Directory.get_directory_path(source_path, current_directory)
+        source_directory = Path_to_Directory.path_to_directory(directory_path, current_directory)
+        destination_directory = Path_to_Directory.path_to_directory(destination_path, current_directory)
 
-        return self._cp_command.execute(args,current_directory)
+        if source_directory is None or destination_directory is None:
+            result = "Invalid source or destination path."
+
+        source_filename = Path_to_Directory.get_filename(source_path)
+
+        source_file = source_directory.get_children_by_name(source_filename)
+
+        result = self._cp_command.execute([destination_directory, source_filename, source_file, destination_path], current_directory)
+
+        if result is not None:
+            print(result)
